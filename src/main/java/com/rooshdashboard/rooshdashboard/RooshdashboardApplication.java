76eeef7ee9728d020f9f59bb594e46cd81469da9
Dorian <507644@student.fontys.nl>
2023-10-30 09:37:05 +0100

    initial commit

---
+++ src/main/java/com/rooshdashboard/rooshdashboard/RooshdashboardApplication.java
@@ -0,0 +1,13 @@
+package com.rooshdashboard.rooshdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RooshdashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RooshdashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rooshdashboard/rooshdashboard/RooshdashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rooshdashboard.rooshdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RooshdashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
