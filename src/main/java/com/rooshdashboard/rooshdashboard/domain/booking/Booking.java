dafc90de5fc81c9d0d1f1d9d675feccb8e07d874
Arthur Jaspar <a.jaspar@student.fontys.nl>
2023-10-31 18:41:10 +0100

    added all necessary annotations to the Booking class

--- src/main/java/com/rooshdashboard/rooshdashboard/domain/booking/Booking.java
+++ src/main/java/com/rooshdashboard/rooshdashboard/domain/booking/Booking.java
@@ -1,13 +1,17 @@
 package com.rooshdashboard.rooshdashboard.domain.booking;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Booking {
     @NotNull
     private Long id;
